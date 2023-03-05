cca233a31f60484f0f2c6e7022a7bc7f5bba19be
Semidime <semidime@protonmail.com>
2023-03-05 15:00:04 +0000

    eventListener on sketchDiv mouseover updated
    function now callsnew  changeColor function.
    
    changeColor calls, assignRGB and assignOpacity.
    changeColor passes the id of the selected
    sketchDiv as an argument to both functions

--- sketch.js
+++ sketch.js
@@ -24,11 +24,13 @@ const sketchDivs = document.querySelectorAll('.sketch-div');
 sketchDivs.forEach(sketchDiv => sketchDiv.addEventListener('mouseover', changeColor))
 }
 
-/* function to control behavious on sketchDiv mouseover */
+/* function to control behavious on sketchDiv mouseover 
+calls selected functions and passes sketchDiv id as an argument*/
 function changeColor() {
+  console.log(this);
   console.log(this.id);
-  assignColor();
-  assignOpacity();  
+  assignColor(this.id);
+  assignOpacity(this.id);
 }
 /* function to add "selected" class to sketch-div on mouseover */
 /* function changeClass() {
@@ -39,32 +41,35 @@ function changeColor() {
 
 /* randomly assign RGB on first mouseover 
 remove if statement to change RGB on every mousover*/
-function assignColor() {
-  console.log(this);
-  console.log(this.id);
-  if (this.style.backgroundColor === "") {
+function assignColor(sdID) {
+  console.log(sdID);
+  const sketchDiv = document.querySelector(`#${sdID}`)
+  
+  if (sketchDiv.style.backgroundColor === "") {
     const R = parseInt(Math.floor(Math.random()*256),10);
     const G = parseInt(Math.floor(Math.random()*256),10);
     const B = parseInt(Math.floor(Math.random()*256),10);
-    this.style.backgroundColor=`rgb(${R},${G},${B})`;
+    sketchDiv.style.backgroundColor=`rgb(${R},${G},${B})`;
   }
-  console.log(this.style.backgroundColor);
+  console.log(sketchDiv.style.backgroundColor);
 }
  
-function assignOpacity() {
-  console.log(this);
-  console.log(this.id);
-  console.log(this.style.opacity);
-  if (this.style.opacity === "") {
-    this.style.opacity = 0.1;
-  } else if (parseFloat(this.style.opacity) > 0 && parseFloat(this.style.opacity) < 1) {
-    const oldOpacity = parseFloat(this.style.opacity);
+/* function to check if opacity has been assigned, if not assign an initial value
+if value already assigned increment value by 0.1 */
+function assignOpacity(sdID) {
+  console.log(sdID);
+  const sketchDiv = document.querySelector(`#${sdID}`)
+
+  if (sketchDiv.style.opacity === "") {
+    sketchDiv.style.opacity = 0.1;
+  } else if (parseFloat(sketchDiv.style.opacity) > 0 && parseFloat(sketchDiv.style.opacity) < 1) {
+    const oldOpacity = parseFloat(sketchDiv.style.opacity);
     const newOpacity = oldOpacity + 0.1;
     console.log(oldOpacity);
     console.log(newOpacity);
-    this.style.opacity = newOpacity;
+    sketchDiv.style.opacity = newOpacity;
   }
-  console.log(this.style.opacity);
+  console.log(sketchDiv.style.opacity);
 }
 
 
